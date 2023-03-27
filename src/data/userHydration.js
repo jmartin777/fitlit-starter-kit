4ab7690df2b4fd1e2ee84df518c1d5399f0f018c
Cade Chaney <cadechaney@live.com>
2023-03-27 16:54:40 -0500

    Initial commit

---
+++ src/data/userHydration.js
@@ -0,0 +1,19 @@
+
+class userHydration {
+  constructor(userID, hydrationInfo) {
+    this.userID = userID
+    this.hydrationInfo = hydrationInfo
+  }
+
+  calculateAllTimeAverageDailyOunces(endDate) {
+    this.userID
+  }
+
+  calculateSingleDayOunces(day) {
+
+  }
+
+  calculateOuncesLastSevenDays(day) {
+
+  }
+}
