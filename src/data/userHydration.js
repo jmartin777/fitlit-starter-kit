b6b6b7e22ae69503ce5f936c1841ac26e19137e3
Cade Chaney <cadechaney@live.com>
2023-03-27 19:38:39 -0500

    Add calculateAllTimeAverageDailyOunces method

--- src/data/userHydration.js
+++ src/data/userHydration.js
@@ -6,17 +6,23 @@ class userHydration {
   }
 
   calculateAllTimeAverageDailyOunces() {
-    
-    
+    const allTimeTotalOunces = this.hydrationInfo
+      .reduce((acc, cur) => {
+        return acc += cur.numOunces
+      }, 0)
+
+    const allTimeAverage = allTimeTotalOunces / hydrationData.length
+
+    return allTimeAverage
   }
 
   calculateSingleDayOunces(day) {
-    
+
     const allDays = this.hydrationInfo.filter(dayInfo => dayInfo.date === day)
     const totalOunces = allDays.reduce((acc, cur) => {
       return acc += cur.numOunces
     }, 0)
-   
+
     return totalOunces
   }
 
