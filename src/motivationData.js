a541b2a2b075444936138d754456acc2d4893358
Isaiah Richardson <capcinematic@gmail.com>
2023-04-13 09:50:30 -0600

    add an array of motivational quotes

---
+++ src/motivationData.js
@@ -0,0 +1,29 @@
+const motivationalQuotes = [
+  "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+ "Dont let yesterday take up too much of today.",
+ "Every day is a second chance.",
+ "Tell the truth and love everyone.",
+ "I am free from sadness.",
+ "I am enough.",
+ "In the beginning it is you, in the middle it is you and in the end it is you.",
+ "I love myself.",
+ "I am present now.",
+ "Inhale the future, exhale the past.",
+ "This too shall pass.",
+ "Yesterday is not today.",
+ "The only constant is change.",
+ "Onward and upward.",
+ "I am the sky, the rest is weather.",
+ "Don't stop when you're tired. Stop when you're done.",
+ "If you don't want to slip up tomorrow, speak the truth today.",
+ "Showing off is the fool's idea of glory.",
+ "Real living is living for others.",
+ "Do the thing you're attracted to doing, That's what you're meant to do. That's who you are.",
+ "The more I train, the luckier I get.",
+ "If you just follow the breadcrumbs of your desires, then strangely they will lure you into the direction of paradise.",
+ "Be the change you want to see in the world.",
+ "When you pay attention to detail, the big picture will take care of itself."
+ ];
+
+ export default motivationalQuotes
+
